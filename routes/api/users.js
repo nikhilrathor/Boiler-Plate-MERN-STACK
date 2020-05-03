2dc2fb27c21cfef9e7616c9c0c440429dfcc4d6a
nikhilrathor <nikhilrathor01@gmail.com>
2020-05-03 16:49:03 +0530

    Logout

--- routes/api/users.js
+++ routes/api/users.js
@@ -56,7 +56,14 @@ router.post('/login', (req, res) => {
         })
 })
 
-
+router.get('/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).send({
+            success: true
+        })
+    })
+})
 
 
 module.exports = router;
