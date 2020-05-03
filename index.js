8c7cf2f1530b6858d131c92fefa62433696c16a4
nikhilrathor <nikhilrathor01@gmail.com>
2020-05-03 11:45:28 +0530

    Connected to mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb+srv://<username>:<password>@cluster0-duzlk.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(mongoURI,
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 });
 
-app.listen(5000);
+const PORT = 5000;
+
+app.listen(PORT, () => { console.log(`server started at port ${PORT}`) });
