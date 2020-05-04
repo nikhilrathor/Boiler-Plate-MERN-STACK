abcddeccd7f201fba3caa1f8e79634050ef6fd46
nikhilrathor <nikhilrathor01@gmail.com>
2020-05-04 20:30:33 +0530

    basic redux added

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,26 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+import Reducer from './reducers';
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const store = createStore(Reducer, composeEnhancers(
+  applyMiddleware(ReduxThunk, promiseMiddleware)
+));
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ client/src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import user from './user_reducer';
+import item from './item_reducer';
+
+const rootReducer = combineReducers({
+    user,
+    item
+});
+
+export default rootReducer;
